f93cfcfb6bfd286c602390de91b29cc22e474cf1
Alastair Sumner <61240854+Sumner1185@users.noreply.github.com>
2020-10-22 14:32:48 +0100

    Update replaceString.groovy

--- vars/replaceString.groovy
+++ vars/replaceString.groovy
@@ -1,6 +1,6 @@
 def call(String fileName) {
   def text = readFile file: "${fileName}"
 
-  text = text.replaceAll("%BUILD_NUMBER", "${BUILD_NUMBER}")
+  text = text.replaceAll("%BUILD_NUMBER%", "${BUILD_NUMBER}")
   writeFile file: "${fileName}", text: text
 }
