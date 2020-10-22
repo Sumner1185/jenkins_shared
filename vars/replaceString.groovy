58abec4777c12c414e176068155dfcb9147620f7
Alastair Sumner <61240854+Sumner1185@users.noreply.github.com>
2020-10-22 13:25:17 +0100

    Create replaceString.groovy

---
+++ vars/replaceString.groovy
@@ -0,0 +1,6 @@
+def replaceString(String fileName) {
+  def text = readFile file: "${fileName}"
+
+  text = text.replaceAll("%BUILD_NUMBER", "${BUILD_NUMBER}")
+  writeFile file: "${fileName}", text: text
+}
